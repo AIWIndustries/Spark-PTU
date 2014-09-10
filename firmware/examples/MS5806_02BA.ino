11c9c6a7e23ffd7d5c127e0613651887e354904a
Richard Vogel <aiwindustries@gmail.com>
2014-09-09 19:11:21 -0500

    Update MS5806_02BA.ino

--- firmware/examples/MS5806_02BA.ino
+++ firmware/examples/MS5806_02BA.ino
@@ -1,22 +1,33 @@
-#include <Wire.h>
-#include <MS5806_02BA52.h>
+#include "MS5806_02BA52.h"
+
 
 
 void setup()
 {
-  PTU_P.begin();
+    Wire.begin();
+    Serial.begin(115200);
+    while(!Serial.available()) // Just chill until we Rx a character. Don't chill
+    {
+        delay(100);
+    }
+    /******************************************************************************************************************************/
+    delay(100);
+    Serial.println(F("Starting up the Pressure Sensor......."));
+    Serial.println(F(""));
+    delay(10);
+    PTU_P.begin();
 }
 
 void loop()
 {
-  PTU_P.getData();
-  Serial.print(F("Pressure: "));
-  Serial.print(pressure, 2);
-  Serial.println(F(" mBar\n"));
+    PTU_P.getData();
+    Serial.print(F("Pressure: "));
+    Serial.print(pressure, 2);
+    Serial.println(F(" mBar\n"));
   
-  Serial.print(F("P_Sensor Temp: "));
-  Serial.print(p_temperature, 2);
-  Serial.println(F(" degrees C"));
+    Serial.print(F("P_Sensor Temp: "));
+    Serial.print(p_temperature, 2);
+    Serial.println(F(" degrees C"));
   
-  delay(1000);
+    delay(5000);
 }

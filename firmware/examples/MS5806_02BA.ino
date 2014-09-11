804af162949a0617ff10a34844e4da2e085d8300
Richard Vogel <aiwindustries@gmail.com>
2014-09-11 15:27:38 -0500

    Update MS5806_02BA.ino

--- firmware/examples/MS5806_02BA.ino
+++ firmware/examples/MS5806_02BA.ino
@@ -1,16 +1,18 @@
 #include "MS5806_02BA52.h"
 
-
-
 void setup()
 {
     Wire.begin();
     Serial.begin(115200);
-    while(!Serial.available()) // Just chill until we Rx a character. Don't chill
+    
+    //uncomment the block comment below if you want the code to wait for you to enter a keyboard
+    //stroke before it starts.
+    
+    /*while(!Serial.available()) 
     {
         delay(100);
-    }
-    /******************************************************************************************************************************/
+    }*/
+   
     delay(100);
     Serial.println(F("Starting up the Pressure Sensor......."));
     Serial.println(F(""));
@@ -23,11 +25,20 @@ void loop()
     PTU_P.getData();
     Serial.print(F("Pressure: "));
     Serial.print(pressure, 2);
-    Serial.println(F(" mBar\n"));
-  
+    Serial.println(F(" mBar"));
+    float p_Hg = pressure * 0.02952998751; //mBar to inHg conversion
+    Serial.print(F("Pressure: "));
+    Serial.print(p_Hg, 2);
+    Serial.println(F(" inHg\n"));
+    
     Serial.print(F("P_Sensor Temp: "));
     Serial.print(p_temperature, 2);
     Serial.println(F(" degrees C"));
+    float tF = (p_temperature * 1.8) + 32; // C to F conversion
+    Serial.print(F("P_Sensor Temp: "));
+    Serial.print(tF, 2);
+    Serial.println(F(" degrees F\n"));
+    
   
     delay(5000);
 }
